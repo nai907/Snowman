72010c540169fc757fe6c343014c4e8ba70a1348
nai907 <103129387+nai907@users.noreply.github.com>
2023-12-13 21:47:42 +0700

    Delete src directory

--- src/p2/Event.java
+++
@@ -1,9 +0,0 @@
-package p2;
-
-public class Event {
-	
-	public static double calculateDistance(int x1, int y1, int x2, int y2) {
-		return Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
-	}
-
-}
